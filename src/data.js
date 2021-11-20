b8292d6a28a11282177640b2e878f74145f84c3a
coffmanjrp <coffmanjrp@gmail.com>
2021-11-19 16:39:58 -0800

    Create a moc file

---
+++ src/data.js
@@ -0,0 +1,32 @@
+import avatar1 from './img/avatar1.jpg';
+import avatar2 from './img/avatar2.jpg';
+import avatar3 from './img/avatar3.jpg';
+
+const data = [
+  {
+    id: 1,
+    name: 'Sarah Doe',
+    image: avatar1,
+    currency: '500.6 ETH',
+  },
+  {
+    id: 2,
+    name: 'Jane Doe',
+    image: avatar3,
+    currency: '234.5 ETH',
+  },
+  {
+    id: 3,
+    name: 'Jone Doe',
+    image: avatar2,
+    currency: '324.5 ETH',
+  },
+  {
+    id: 4,
+    name: 'Sarah Doe',
+    image: avatar1,
+    currency: '109.2 ETH',
+  },
+];
+
+export default data;
